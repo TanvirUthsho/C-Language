932668097b086dc04d956515c6093dc63af4e09a
TanvirUthsho <tanvir.uthsho@northsouth.edu>
2020-09-08 22:34:59 +0600

    Screenshot Added

--- Assignment on Array/3_finding_duplicate_number_with_index_number_itself.c
+++ Assignment on Array/3_finding_duplicate_number_with_index_number_itself.c
@@ -1,39 +1,29 @@
-/* Write a program that searches for duplicate numbers in an array. If the program finds duplication
-of numbers, it will print the index of those numbers and the numbers themselves as well.*/
-
+/* 3. Write a program that searches for duplicate numbers in an array. If the
+program finds duplication of numbers, it will print the index of those numbers
+ and the numbers themselves as well.*/
 #include <stdio.h>
-
-void findDup(int arr[], int n);
+#include <math.h>
 int main()
 {
-    int size;
-    printf("Enter array range:");
-    scanf("%d", &size);
-    int arr[size];
-    for (int i = 0; i < size; i++)
-    {
-        scanf("%d", &arr[i]);
-    }
-    findDup(arr, size);
-    return 0;
-}
-
-void findDup(int arr[], int n)
-{
-    int i, j, count = 0;
+    int i, n, j;
+    printf("Enter array length:");
+    scanf("%d", &n);
+    int a[n];
     for (i = 0; i < n; i++)
     {
-        int a = 0, temp[n];
+        printf("Enter Numbers:");
+        scanf("%d", &a[i]); // Take user input from array.
+    }
+    printf("Duplicate elements in are:");
+    for (i = 0; i < n; i++)
+    {
         for (j = i + 1; j < n; j++)
-        {
-            if (arr[i] == arr[j])
-                ;
+        { // It will depend the value of i position.
+            if (a[i] == a[j])
             {
-                temp[a] = arr[i];
-                count++;
-                arr++;
+                printf("%d", a[i]);
             }
         }
     }
-    for (arr = 0; arr < n;)
+    return 0;
 }
